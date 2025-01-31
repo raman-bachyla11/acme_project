2e60fa5f93ed44dd99277297a461ff0459cc1772
Raman Bachyla <raman.bachyla@gmail.com>
2025-01-30 20:56:56 -0500

    add congratulation-under-post functionality

--- acme_project/birthday/urls.py
+++ acme_project/birthday/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     path('', views.BirthdayCreateView.as_view(), name='create'),
     path('list/', views.BirthdayListView.as_view(), name='list'),
     path('<int:pk>/', views.BirthdayDetailView.as_view(), name='detail'),
+    path('<int:pk>/comment/', views.add_comment, name='add_comment'),
+    path(
+        '<int:pk>/delete/', views.BirthdayDeleteView.as_view(), name='delete'
+    ),
     path('<int:pk>/edit/', views.BirthdayUpdateView.as_view(), name='edit'),
-    path('<int:pk>/delete/', views.BirthdayDeleteView.as_view(), name='delete')
 ]
--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.decorators import login_required
 from django.views.generic import (
     CreateView, DeleteView, DetailView, ListView, UpdateView
 )
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
 
-from .forms import BirthdayForm
-from .models import Birthday
+from .forms import BirthdayForm, CongratulationForm
+from .models import Birthday, Congratulation
 from .utils import calculate_birthday_countdown
 
 
@@ -51,4 +53,31 @@ class BirthdayDetailView(DetailView):
         context['birthday_countdown'] = calculate_birthday_countdown(
             self.object.birthday
         )
+        # Записываем в переменную form пустой объект формы.
+        context['form'] = CongratulationForm()
+        # Запрашиваем все поздравления для выбранного дня рождения.
+        context['congratulations'] = (
+            # Дополнительно подгружаем авторов комментариев,
+            # чтобы избежать множества запросов к БД.
+            self.object.congratulations.select_related('author')
+        )
         return context
+
+
+@login_required
+def add_comment(request, pk):
+    # Получаем объект дня рождения или выбрасываем 404 ошибку.
+    birthday = get_object_or_404(Birthday, pk=pk)
+    # Функция должна обрабатывать только POST-запросы.
+    form = CongratulationForm(request.POST)
+    if form.is_valid():
+        # Создаём объект поздравления, но не сохраняем его в БД.
+        congratulation = form.save(commit=False)
+        # В поле author передаём объект автора поздравления.
+        congratulation.author = request.user
+        # В поле birthday передаём объект дня рождения.
+        congratulation.birthday = birthday
+        # Сохраняем объект в БД.
+        congratulation.save()
+    # Перенаправляем пользователя назад, на страницу дня рождения.
+    return redirect('birthday:detail', pk=pk)
